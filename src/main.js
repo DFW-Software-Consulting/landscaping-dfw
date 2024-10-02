543a8967fe8679def67adaf38b7f396527379564
JC <j.c.ashley4363@gmail.com>
2024-10-02 08:14:50 -0500

    upated html file with dfw garending page

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import './assets/tailwind.css' // <-- Add this line to import Tailwind styles
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
