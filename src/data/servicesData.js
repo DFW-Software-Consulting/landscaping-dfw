d12f3c02aa8f3504e2c5c854b1de664aef8f2527
JC <j.c.ashley4363@gmail.com>
2024-10-03 12:07:46 -0500

    updated services to keep the data sepereate for reusebleness

---
+++ src/data/servicesData.js
@@ -0,0 +1,30 @@
+import serviceImage1 from '../assets/img/service-1.jpg'
+import serviceIcon1 from '../assets/img/icon/icon-3.png'
+import serviceImage2 from '../assets/img/service-2.jpg'
+import serviceIcon2 from '../assets/img/icon/icon-6.png'
+import serviceImage3 from '../assets/img/service-3.jpg'
+import serviceIcon3 from '../assets/img/icon/icon-5.png'
+
+export const services = [
+  {
+    title: 'Landscaping',
+    description:
+      'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
+    image: serviceImage1,
+    icon: serviceIcon1
+  },
+  {
+    title: 'Pruning Plants',
+    description:
+      'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
+    image: serviceImage2,
+    icon: serviceIcon2
+  },
+  {
+    title: 'Irrigation & Drainage',
+    description:
+      'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
+    image: serviceImage3,
+    icon: serviceIcon3
+  }
+]
--- src/sections/Services.vue
+++ src/sections/Services.vue
@@ -82,12 +82,7 @@
 
 <script>
 import Carousel from '@/components/Carousel.vue'
-import serviceImage1 from '../assets/img/service-1.jpg'
-import serviceIcon1 from '../assets/img/icon/icon-3.png'
-import serviceImage2 from '../assets/img/service-2.jpg'
-import serviceIcon2 from '../assets/img/icon/icon-6.png'
-import serviceImage3 from '../assets/img/service-3.jpg'
-import serviceIcon3 from '../assets/img/icon/icon-5.png'
+import { services } from '@/data/servicesData' // Importing data from a separate file
 
 export default {
   name: 'ServicesSection',
@@ -96,29 +91,7 @@ export default {
   },
   data() {
     return {
-      services: [
-        {
-          title: 'Landscaping',
-          description:
-            'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
-          image: serviceImage1,
-          icon: serviceIcon1
-        },
-        {
-          title: 'Pruning Plants',
-          description:
-            'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
-          image: serviceImage2,
-          icon: serviceIcon2
-        },
-        {
-          title: 'Irrigation & Drainage',
-          description:
-            'Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet.',
-          image: serviceImage3,
-          icon: serviceIcon3
-        }
-      ]
+      services
     }
   }
 }
