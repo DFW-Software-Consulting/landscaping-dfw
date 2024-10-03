8fd7263f0b9dd39b1518ad42015e0d8dce944c6e
JC <j.c.ashley4363@gmail.com>
2024-10-03 12:25:27 -0500

    added projectdata file to make project.vue moore reuseabe

---
+++ src/data/projectData.js
@@ -0,0 +1,33 @@
+import projectImage1 from '../assets/img/service-1.jpg'
+import projectImage2 from '../assets/img/service-2.jpg'
+import projectImage3 from '../assets/img/service-3.jpg'
+import projectImage4 from '../assets/img/service-4.jpg'
+import projectImage5 from '../assets/img/service-5.jpg'
+import projectImage6 from '../assets/img/service-6.jpg'
+
+export const projects = [
+  {
+    title: 'Landscaping',
+    image: projectImage1
+  },
+  {
+    title: 'Pruning Plants',
+    image: projectImage2
+  },
+  {
+    title: 'Irrigation & Drainage',
+    image: projectImage3
+  },
+  {
+    title: 'Garden Maintenance',
+    image: projectImage4
+  },
+  {
+    title: 'Green Technology',
+    image: projectImage5
+  },
+  {
+    title: 'Urban Gardening',
+    image: projectImage6
+  }
+]
--- src/sections/Projects.vue
+++ src/sections/Projects.vue
@@ -8,19 +8,6 @@
         <h1 class="text-5xl font-bold mb-5">Some Of Our Wonderful Projects</h1>
       </div>
 
-      <!-- Filter Buttons -->
-      <!-- <div class="text-center mb-10">
-        <ul class="flex justify-center space-x-4">
-          <li class="cursor-pointer px-4 py-2 bg-primary text-white rounded-md">All</li>
-          <li class="cursor-pointer px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md">
-            Complete Projects
-          </li>
-          <li class="cursor-pointer px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md">
-            Ongoing Projects
-          </li>
-        </ul>
-      </div> -->
-
       <!-- Carousels on small screens and grid on medium and larger screens -->
       <div>
         <!-- First Carousel (first three projects) visible on small screens -->
@@ -108,12 +95,7 @@
 
 <script>
 import Carousel from '@/components/Carousel.vue'
-import projectImage1 from '../assets/img/service-1.jpg'
-import projectImage2 from '../assets/img/service-2.jpg'
-import projectImage3 from '../assets/img/service-3.jpg'
-import projectImage4 from '../assets/img/service-4.jpg'
-import projectImage5 from '../assets/img/service-5.jpg'
-import projectImage6 from '../assets/img/service-6.jpg'
+import { projects } from '@/data/projectData'
 
 export default {
   name: 'ProjectsSection',
@@ -122,32 +104,7 @@ export default {
   },
   data() {
     return {
-      projects: [
-        {
-          title: 'Landscaping',
-          image: projectImage1
-        },
-        {
-          title: 'Pruning Plants',
-          image: projectImage2
-        },
-        {
-          title: 'Irrigation & Drainage',
-          image: projectImage3
-        },
-        {
-          title: 'Garden Maintenance',
-          image: projectImage4
-        },
-        {
-          title: 'Green Technology',
-          image: projectImage5
-        },
-        {
-          title: 'Urban Gardening',
-          image: projectImage6
-        }
-      ]
+      projects
     }
   }
 }
